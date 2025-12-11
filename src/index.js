6cd232892f623850a969647b371c209838be7547
David13-117 <d.castillom55@gmail.com>
2025-12-11 03:41:02 -0500

    Spring 2 Menu

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import path from 'path';
-import expresslayouts from 'express-ejs-layouts';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.set('view engine', 'ejs');
-app.set('views','./src/views');
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "/public")));
+
+const publicPath = path.join(__dirname, '..', 'public');
+console.log("Public serving from:", publicPath);
+
+app.use(express.static(publicPath));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/login', (req, res) => res.render('login'));
 app.get('/recuperar', (req, res) => res.render('recuperarcontra'));
+app.get('/principal', (req, res) => res.render('principal'));
+app.get('/entrega', (req, res) => res.render('entregas'));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
--- src/routers/recuperarcontra.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/recuperarcontra", (req, res) => {
-    res.render("recuperarcontra", { title: "Recuperar Contraseña" });
-});
-
-export default router;
